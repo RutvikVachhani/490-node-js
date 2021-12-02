a46d246bf65b8ddfb51b411362ca71205583d939
Gerardo Baron <gerardo.baron@hotmail.com>
2021-12-01 20:08:27 -0800

    courses

--- reqpdf.js
+++ reqpdf.js
@@ -1,10 +1,11 @@
 //Connect to database
 var mysql = require("mysql");
+const con = require("./db_connect");
 const { end } = require("./db_connect");
 var db_connect = require("./db_connect");
 
 var prereqChain1 = ["MATH 150A", "MATH 150B", "MATH 262", "MATH 482"];
-var lastcourse;
+var lastcourse = 0;
 for (var i = 0; i < prereqChain1.length; i++) {
   var Cno = prereqChain1[i].substring(5, prereqChain1[i].length);
   // console.log(Cno);
@@ -20,13 +21,16 @@ for (var i = 0; i < prereqChain1.length; i++) {
     //console.log(result);
     if (!result[0]) {
       console.log("hi");
-      lastcourse = i;
-      return lastcourse;
+      if (lastcourse == 0) {
+        lastcourse = i;
+      }
+      // console.log(lastcourse);
     } else {
       if (
         (result[0].Grade =
           "A" || "A-" || "B+" || "B" || "B-" || "C+" || "C" || "IP")
       ) {
+        console.log(i);
         console.log(result[0].CourseNumber);
         // console.log("Below is Cno");
         // console.log(Cno);
@@ -34,7 +38,9 @@ for (var i = 0; i < prereqChain1.length; i++) {
       }
     }
   });
+  // return lastcourse;
 }
-console.log(lastcourse);
 db_connect.end();
+console.log("outside");
+console.log(lastcourse);
 //console.log(prereqChain1[lastCourse + 1]);
